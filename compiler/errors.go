0b8cdbcd8c988bca2ff6bccda6a59905d4a825dc
Gon <gspina1995@gmail.com>
2022-04-05 13:57:43 -0300

    Added error codes for easier testing

--- compiler/errors.go
+++ compiler/errors.go
@@ -5,62 +5,89 @@ import (
 	"fmt"
 )
 
-func compileErr(t Token, message string) error {
+type ErrCode string
+
+const (
+	UnexpectedEOFErrCode              = "Unexpected end of file"
+	ExpectedAssignmentOperatorErrCode = "Expected assignment operator"
+	InvalidTypeErrCode                = "Invalid variable type"
+	UnexpectedTokenErrCode            = "Unexpected token"
+	ExpectedExpressionErrCode         = "Expected expression"
+	ExpectedRightParenErrCode         = "Expected right parenthesis"
+	ExpectedThenErrCode               = "Expected then"
+	ExpectedEndIfErrCode              = "Expected end if"
+	ExpectedTimesAfterLoopErrCode     = "Expected times"
+	ExpectedEndLoopAfterLoopErrCode   = "Expected end loop"
+	UndefinedVariableErrCode          = "Undefined variable"
+	BlockIsTooLargeErrCode            = "Block is too large"
+)
+
+func compileErr(t Token, message string, code ErrCode) error {
 	return errors.New(fmt.Sprintf(
 		"Line %v Column %v: %s'",
 		t.line,
 		t.column,
-		message,
+		fmt.Sprintf("%s. ErrCode: %s", message, string(code)),
 	))
 }
 
 func unexpectedEndOfFileErr(t Token) error {
-	return compileErr(t, "unexpected end of file")
+	return compileErr(t, "unexpected end of file", UnexpectedEOFErrCode)
 }
 
 func expectedAssignmentOperatorErr(t Token) error {
-	return compileErr(t, "expected assignment operator '<-'")
+	return compileErr(t, "expected assignment operator '<-'", ExpectedAssignmentOperatorErrCode)
 }
 
-func assignErr(t Token, expected varType, got varType) error {
+func invalidTypeErr(t Token, expected varType, got varType) error {
 	return compileErr(t, fmt.Sprintf(
 		"cannot assign '%s' value to variable of type '%s'",
 		got.String(),
 		expected.String(),
-	))
+	), InvalidTypeErrCode)
 }
 
 func unexpectedTokenErr(t Token) error {
 	return compileErr(t, fmt.Sprintf(
 		"unexpected token '%s'",
 		t.lexeme,
-	))
+	), UnexpectedTokenErrCode)
 }
 
 func expectedExpressionErr(t Token) error {
-	return compileErr(t, "expected expression")
+	return compileErr(t, "expected expression", ExpectedExpressionErrCode)
 }
 
 func expectedRightParenthesisErr(t Token) error {
-	return compileErr(t, "expected closing parenthesis")
+	return compileErr(t, "expected closing parenthesis", ExpectedRightParenErrCode)
 }
 
 func expectedThenErr(t Token) error {
-	return compileErr(t, "expected 'then' after expression")
+	return compileErr(t, "expected 'then' after expression", ExpectedThenErrCode)
 }
 
 func expectedEndIfErr(t Token) error {
-	return compileErr(t, "expected 'end if' after block")
+	return compileErr(t, "expected 'end if' after block", ExpectedEndIfErrCode)
 }
 
 func expectedTimesErr(t Token) error {
-	return compileErr(t, "expected 'times' after expression")
+	return compileErr(t, "expected 'times' after expression", ExpectedTimesAfterLoopErrCode)
 }
 
 func expectedEndLoopErr(t Token) error {
-	return compileErr(t, "expected 'end loop' after block")
+	return compileErr(t, "expected 'end loop' after block", ExpectedEndLoopAfterLoopErrCode)
 }
 
 func blockIsTooLargeErr(t Token) error {
-	return compileErr(t, "block is too large")
+	return compileErr(t, "block is too large", BlockIsTooLargeErrCode)
+}
+
+func undefinedVariableErr(t Token, name string) error {
+	return compileErr(t,
+		fmt.Sprintf("cannot evaluate '%s' because it wasn't assigned before: %s <- Value",
+			name,
+			name,
+		),
+		UndefinedVariableErrCode,
+	)
 }
